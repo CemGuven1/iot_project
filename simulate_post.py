eb69dab7001a00dc56a73182b790d2145bcb72e8
CemGuven1 <cguven20@ku.edu.tr>
2024-09-16 11:00:35 +0300

    charts updated to display only the recent data

--- simulate_post.py
+++ simulate_post.py
@@ -5,11 +5,12 @@ import json
 url = 'http://127.0.0.1:8000/api/data-entry/'
 
 # Sample data to be posted
+
 data = {
     'device_id': '12345',
-    'acceleration_x': 0.1,
-    'acceleration_y': 0.2,
-    'acceleration_z': 0.3,
+    'acceleration_x': 3,
+    'acceleration_y': 4,
+    'acceleration_z': 2,
     'gyro_x': 0.01,
     'gyro_y': 0.02,
     'gyro_z': 0.03,
@@ -17,7 +18,22 @@ data = {
     'battery': 95.0
 }
 
+data1 = {
+    'device_id': '12345',
+    'acceleration_x': 6,
+    'acceleration_y': 6,
+    'acceleration_z': 6,
+    'gyro_x': 0.01,
+    'gyro_y': 0.02,
+    'gyro_z': 0.03,
+    'temperature': 22.5,
+    'battery': 95.0
+}
+
+
+
 response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+response = requests.post(url, data=json.dumps(data1), headers={'Content-Type': 'application/json'})
 
 print(response.status_code)
 print(response.json())
